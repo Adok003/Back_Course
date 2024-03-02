842dfbe34c4743b633eb7ca312b617baec8b866c
Adok003 <timuruly765@gmail.com>
2024-03-02 20:11:08 +0500

    test application for deploy

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from "mongoose";
+import router from "./Router.js";
+import fileUpload from 'express-fileupload'
+
+const PORT = 5000;
+const MONGO_URL = 'mongodb+srv://root:root@cluster0.fwtgckq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static('static'))
+app.use(fileUpload({}))
+app.use("/api", router)
+
+
+async function startApp() {
+    try {
+        await mongoose.connect(MONGO_URL)
+        app.listen(PORT, () => console.log("Server Started"))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+startApp()
