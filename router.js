c59367046622bd0e8b626dfa3ab93cfb8031722e
Adok003 <timuruly765@gmail.com>
2024-03-03 21:31:36 +0600

    Add user table and endpoints

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 import Router from "express";
 import PostController from "./PostController.js";
+import UserController from "./UserController.js";
 
 const router = new Router()
 
@@ -9,5 +10,11 @@ router.get("/posts", PostController.getAll)
 router.get("/posts/:id", PostController.getOne)
 router.put("/posts", PostController.update)
 router.delete("/posts/:id", PostController.delete)
+
+router.post("/user", UserController.create)
+router.get("/user", UserController.getAll)
+router.get("/user/:id", UserController.getOne)
+router.put("/users", UserController.update)
+router.delete("/user/:id", UserController.delete)
 
 export default router;
